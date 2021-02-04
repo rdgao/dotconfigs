fabeeed57fb86c091f99965104b83e79a3780ba8
rdgao <r.dg.gao@gmail.com>
2021-02-04 15:47:58 +0100

    got bash_profile to work with zshrc

--- make_links.sh
+++ make_links.sh
@@ -3,3 +3,6 @@
 # git stuff
 ln -s ~/Documents/code/misc/dotconfigs/git/.gitconfig ~/.gitconfig
 ln -s ~/Documents/code/misc/dotconfigs/git/.gitignore_global ~/.gitignore_global
+
+# bash
+ln -s ~/Documents/code/misc/dotconfigs/bash/.bash_profile ~/.bash_profile
